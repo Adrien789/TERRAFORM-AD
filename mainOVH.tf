ea41229d812763837f418173c45fa9ff1c0af26b
Adrienchl <adrien.choulat@psi.local>
2026-02-06 14:22:33 +0100

    modification mainOVH.tf

--- mainOVH.tf
+++ mainOVH.tf
@@ -9,16 +9,16 @@ terraform {
 
 provider "ovh" {
   endpoint           = "ovh-eu"
-  application_key    = afc5883b59c00038
-  application_secret = c994f8168c333e24c3e166b9cd9190a3
-  consumer_key       = dea278b169b4698ebedbb28d521b8d5b
+  application_key    = "afc5883b59c00038"
+  application_secret = "c994f8168c333e24c3e166b9cd9190a3"
+  consumer_key       = "dea278b169b4698ebedbb28d521b8d5b"
 }
 
 # Création d'un container de stockage (Object Storage)
-resource "ovh_cloud_project_container" "mon_stockage" {
+resource "ovh_cloud_project_storage_container" "mon_stockage" {
   service_name = "TON_PROJECT_ID" # L'ID de ton projet Public Cloud OVH
   region       = "GRA"            # Région Gravelines
   name         = "stockage-devops-output"
-  type         = "private" # Peut être 'public' ou 'private'
+  type         = "private"        # Peut être 'public' ou 'private'
 }
 
