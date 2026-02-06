e0b851a22c1f836538ca87ef2b541500ddb6c842
Adrienchl <adrien.choulat@psi.local>
2026-02-06 14:43:03 +0100

    modification mainOVH.tf

--- mainOVH.tf
+++ mainOVH.tf
@@ -15,12 +15,10 @@ provider "ovh" {
 }
 
 # Création d'un container de stockage (Object Storage)
-resource "ovh_cloud_project_storage" "my-bucket" {
- service_name = "fec35d4f58194e9f840d7de8f491f937" # Replace with your OVHcloud project ID
- region_name = "GRA" # Replace with the desired region in uppercase.
-  name = "object-storage-simple"
-  versioning = {
-    status = "enabled"
-  }
+resource "ovh_cloud_project_storage_container" "mon_stockage" {
+  service_name = "fec35d4f58194e9f840d7de8f491f937" # <--- ID copié de ta capture
+  region       = "GRA"
+  name         = "stockage-devops-output"
 }
+
 
